5c954e306e0d47ad77f321de65b52afbab28c932
Sumit Suthar <ssuthar@newrelic.com>
2024-05-17 13:28:59 +0530

    rce added

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-
+const cp = require('child_process');
 app.get('/', (req, res) => {
   req.headers.port = [3000];
   console.log("req.headers:", req.headers);
   res.send('Hello World!');
+});
+
+app.get('/rce', (req, res) => {
+  req.headers.port = [3000];
+  var payload = req.query["payload"];
+  let result = cp.execSync(payload);
+  res.send(result.toString());
 });
 
 const port =3000;
